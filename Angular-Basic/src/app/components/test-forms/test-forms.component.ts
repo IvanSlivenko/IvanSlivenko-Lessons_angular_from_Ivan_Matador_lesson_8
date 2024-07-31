5e94d0d893dded794c06bed3aff5695bfc9e031e
IvanSlivenko <ivan8822@ukr.net>
2024-07-31 20:31:49 +0300

    31-07-2024-20-31

--- Angular-Basic/src/app/components/test-forms/test-forms.component.ts
+++ Angular-Basic/src/app/components/test-forms/test-forms.component.ts
@@ -1,14 +1,40 @@
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { Component } from '@angular/core';
+import { F } from '@angular/cdk/keycodes';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-test-forms',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [
+            MatCardModule, 
+            MatButtonModule, 
+            MatGridListModule, 
+            MatInputModule, 
+            MatFormFieldModule, 
+            FormsModule, 
+            ReactiveFormsModule
+          ],
   templateUrl: './test-forms.component.html',
-  styleUrl: './test-forms.component.scss'
+  styleUrl: './test-forms.component.scss',
+  
 })
 export class TestFormsComponent {
-
+  form2: FormGroup = new FormGroup({
+    name: new FormControl('Name'),
+    text: new FormControl('Text')
+  });
+  sendForm(form: any){
+    // console.log(form);
+    console.log(form.value);
+    // console.log(form.value.text);
+    // console.log(form.value.name);
+    // form.value.text="";
+    
+  }
 }
